0a152f2166faacb08ab5e9a431deed9759d4fe00
Stanislav <stalevs@gmail.com>
2025-08-16 17:50:04 +0200

    basic html+css+js add

--- index.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('h1').innerHTML = "There is no spoon!";
-document.body.style.backgroundColor = "green";
---
+++ src/index.js
@@ -0,0 +1,2 @@
+document.querySelector('h1').innerText = "There is no spoon!";
+document.body.style.backgroundColor = "green";
