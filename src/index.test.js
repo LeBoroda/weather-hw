d47bc6337c5af5d6b989e7bd00335bd7812d1745
Stanislav <stalevs@gmail.com>
2025-08-16 21:05:30 +0200

    implement tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,30 @@
-describe('Mock test', () => {
-  test('Mock test', () => {
-    expect(1 + 1).toBe(2);
+import fs from 'fs';
+import path from 'path';
+
+describe('Sample page test', () => {
+  beforeEach(() => {
+    document.documentElement.innerHTML = fs.readFileSync(
+      path.resolve(__dirname, '../index.html'),
+      'utf8',
+    );
+    import('./index.js');
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test('Element exists', () => {
+    expect(document.querySelector('h1')).not.toBe(null);
+  });
+  test('Header text changed', () => {
+    const h1 = document.querySelector('h1');
+    expect(h1.innerText).toBe('There is no spoon!');
+  });
+  test('Header color changed', () => {
+    const h1 = document.querySelector('h1');
+    expect(h1.style.color).toBe('blue');
+  });
+  test('Background color changed', () => {
+    expect(document.body.style.backgroundColor).toBe('green');
   });
 });
